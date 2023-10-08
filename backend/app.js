690e51763aa0cc3f8809fc7311c20090b75fb4c5
MusyokaMark <markmusyoka1999@email.com>
2023-10-08 15:07:05 -0700

    final code

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 // app.use(express.json());
 // app.use(cookieParser());
-// app.use(express.json());
+app.use("/", express.static("uploads"));
 app.use("/", (req, res) => {
   res.send("Hello world!");
 });

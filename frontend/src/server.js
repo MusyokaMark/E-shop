66ace23bb4afd2bd03d79cdf038fdb8f8bc100ae
MusyokaMark <markmusyoka1999@email.com>
2023-12-05 04:14:33 +0300

    backend url updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "e-shop-7mpa-musyokas-projects.vercel.app/api/v2";
-export const backend_url = "e-shop-7mpa-musyokas-projects.vercel.app";
+export const server = "e-shop-musyokas-projects.vercel.app/api/v2";
+export const backend_url = "e-shop-musyokas-projects.vercel.app";
 
 
